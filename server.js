3d809b249db61b4a6aab70afc6ebb3ce3c58077b
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-09-02 23:32:56 +0300

    feat: connected app in server.js to use blocks route.
    test: tested the /api/blocks/test and works

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// Project imports
+const blocks = require("./routes/api/blocks");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,6 +12,9 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("PRIVATE BLOCKCHAIN PRIVATE API");
 });
+
+// ROUTES
+app.use("/api/blocks", blocks);
 
 const port = process.env.PORT || 5000;
 
